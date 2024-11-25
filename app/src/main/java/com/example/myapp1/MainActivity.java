b507443e105f52279282bd5a3963603473e4560c
layan123-create <layanawad123123@gmail.com>
2024-11-25 18:24:27 +0200

    first commit

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapp1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView tvWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvWelcome=findViewById(R.id.textView);
     }
-}
+    public void change_text(View view) {
+        tvWelcome.setText("This is my first app");
+    }
+
+    public void finishApp(View view) {
+        finish();
+        System.exit(0);
+    }
+    }
